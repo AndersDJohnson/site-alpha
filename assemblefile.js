eff3e17af7450a13a2c27d2d83cd5237d556ba58
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-09-07 03:40:33 -0500

    Update assemble process.

--- assemblefile.js
+++ assemblefile.js
@@ -1,9 +1,26 @@
 var assemble = require('assemble');
+var path = require('path');
+assemble.engine('html', require('engine-handlebars'));
+// assemble.engine('hbs', require('engine-handlebars'));
+// var extname = require('gulp-extname');
 
-assemble.enable('minimal config');
+/**
+ * https://github.com/assemble/assemble/issues/687
+ */
+assemble.option('renameKey', function (fp) {
+  var dirname = path.dirname(fp);
+  var basename = path.basename(fp);
+  var last = path.basename(dirname);
+  return path.join(last, basename);
+});
 
+/**
+ * Ext issues: https://github.com/assemble/assemble/issues/642
+ */
 assemble.task('html', function() {
   assemble.src(['templates/**/*.html'])
+  // assemble.src(['templates/**/*.hbs'])
+    // .pipe(extname('.html'))
     .pipe(assemble.dest('dist/'));
 });
 
