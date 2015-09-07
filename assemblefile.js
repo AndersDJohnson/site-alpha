1a1b52adb01a7156e4a477be91f8492c4c859138
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-09-07 13:54:23 -0500

    Move to layouts. Relative link and assets URLs.

--- assemblefile.js
+++ assemblefile.js
@@ -3,6 +3,9 @@ var path = require('path');
 assemble.engine('html', require('engine-handlebars'));
 // assemble.engine('hbs', require('engine-handlebars'));
 // var extname = require('gulp-extname');
+// assemble.engine('html', require('engine-lodash'));
+
+// console.log(assemble.engines['.html'])
 
 /**
  * https://github.com/assemble/assemble/issues/687
@@ -14,19 +17,47 @@ assemble.option('renameKey', function (fp) {
   return path.join(last, basename);
 });
 
+
+assemble.layouts('layouts/*.hbs');
+// assemble.layouts('layouts/*.tmpl');
+
+// assemble.set('layout', 'layouts/default.hbs');
+// // assemble.set('layout', 'layouts/default');
+// assemble.options({
+//     layout: 'layouts/default.hbs'
+// });
+// assemble.options.layout = 'layouts/default.hbs';
+// assemble.option('layout', 'layouts/default.hbs');
+
+// console.log(assemble.cache);
+// console.log(assemble.options);
+
+var relativeDest = require('relative-dest');
+
+assemble.helper('relativeUrl', function (from, to, opts) {
+  var dest = opts.data.root.dest.dest;
+  return relativeDest(from, dest + '/' + to);
+});
+
 /**
  * Ext issues: https://github.com/assemble/assemble/issues/642
  */
 assemble.task('html', function() {
-  assemble.src(['templates/**/*.html'])
+  assemble.option('assets', 'dist');
+  assemble.src(['templates/**/*.html'], {
+    // layout: 'layouts/default.tmpl',
+    layout: 'layouts/default.hbs',
+    // layoutDelims: ['!_', '_!'],
+    assets: 'dist'
+  })
   // assemble.src(['templates/**/*.hbs'])
     // .pipe(extname('.html'))
-    .pipe(assemble.dest('dist/'));
+    .pipe(assemble.dest('dist'));
 });
 
 assemble.task('public', function () {
   assemble.src(['public/**/*'])
-    .pipe(assemble.dest('dist/'));
+    .pipe(assemble.dest('dist'));
 });
 
 assemble.task('default', ['html', 'public']);
