8b90fd6246f2e0da62f3e57e82b3167ed31fcf57
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-09-07 02:56:39 -0500

    Assemble minimal config for globs.

--- assemblefile.js
+++ assemblefile.js
@@ -1,12 +1,14 @@
 var assemble = require('assemble');
 
+assemble.enable('minimal config');
+
 assemble.task('html', function() {
-  assemble.src(['templates/*.html', 'templates/**/*.html'])
+  assemble.src(['templates/**/*.html'])
     .pipe(assemble.dest('dist/'));
 });
 
 assemble.task('public', function () {
-  assemble.src(['public/*', 'public/**/*'])
+  assemble.src(['public/**/*'])
     .pipe(assemble.dest('dist/'));
 });
 
