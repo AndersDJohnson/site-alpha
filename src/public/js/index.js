5a958d4929601000aa61797337a4e6e6d70f0e2d
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-09-08 00:27:14 -0500

    Radial gradient top mouse tracking.

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -9,6 +9,35 @@ $(function () {
     positionProperty: 'transform'
   });
 
+  var normalizeOffsets = function (e) {
+    var offset = $(e.currentTarget).offset();
+    return {
+      left: e.pageX - offset.left,
+      top: e.pageY - offset.top
+    };
+  };
+
+  var ratioOffsets = function (e) {
+    var normals = normalizeOffsets(e);
+    var $target = $(e.currentTarget);
+    return {
+      left: normals.left / $target.width(),
+      top: normals.top / $target.height()
+    };
+  };
+
+
+  $('.top').on('mousemove', function (e) {
+    var ratios = ratioOffsets(e);
+    var left = ratios.left * 100;
+    var top = ratios.top * 100;
+    var $target = $(e.currentTarget);
+    $target.css({
+      background: 'radial-gradient(circle at ' + left + '% ' + top + '%, ' +
+        '#FF5573 0%, purple 100%)'
+    });
+  });
+
 
   // var i = 0;
   // var a = 5;
