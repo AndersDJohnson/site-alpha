bb80b3e2ff37a8a8eadce2b5adc9c53b43d597d4
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-09-08 00:08:54 -0500

    Add Stellar for parallax top.

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,10 +1,21 @@
 
+$(function () {
 
-// var i = 0;
-// var a = 5;
-// setInterval(function () {
-//   a = a > 0 && i > 89 ? -a : (a < 0 && i < 11 ? -a : a);
-//   i += a;
-//   document.querySelector('.top').style.background = 'radial-gradient(ellipse at ' + i + '% 100%, #FF5573 0%, purple 100%)';
-// }, 100);
 
+  $.stellar({
+    responsive: true,
+    horizontalScrolling: false,
+    hideDistantElements: false,// doesn't work for flow content
+    positionProperty: 'transform'
+  });
+
+
+  // var i = 0;
+  // var a = 5;
+  // setInterval(function () {
+  //   a = a > 0 && i > 89 ? -a : (a < 0 && i < 11 ? -a : a);
+  //   i += a;
+  //   document.querySelector('.top').style.background = 'radial-gradient(ellipse at ' + i + '% 100%, #FF5573 0%, purple 100%)';
+  // }, 100);
+
+});
